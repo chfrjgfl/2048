f6afac0c491ee312c4cfe287a46fd615b04e424a
chfrjgfl <104519853+chfrjgfl@users.noreply.github.com>
2022-05-07 10:03:29 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,7 +1,51 @@
+/*
+Instructions
+Given a mathematical expression as a string you must return the result as a number.
+
+Numbers
+Number may be both whole numbers and/or decimal numbers. The same goes for the returned result.
+
+Operators
+You need to support the following mathematical operators:
+
+Multiplication *
+Division / (as floating point division)
+Addition +
+Subtraction -
+Operators are always evaluated from left-to-right, and * and / must be evaluated before + and -.
+
+Parentheses
+You need to support multiple levels of nested parentheses, ex. (2 / (2 + 3.33) * 4) - -6
+
+Whitespace
+There may or may not be whitespace between numbers and operators.
+
+An addition to this rule is that the minus sign (-) used for negating numbers and parentheses will never be separated by whitespace. I.e all of the following are valid expressions.
+
+1-1    // 0
+1 -1   // 0
+1- 1   // 0
+1 - 1  // 0
+1- -1  // 2
+1 - -1 // 2
+1--1   // 2
+
+6 + -(4)   // 2
+6 + -( -4) // 10
+And the following are invalid expressions
+
+1 - - 1    // Invalid
+1- - 1     // Invalid
+6 + - (4)  // Invalid
+6 + -(- 4) // Invalid
+Validation
+You do not need to worry about validation - you will only receive valid mathematical expressions following the above rules.
+
+Restricted APIs
+NOTE: Both eval and Function are disabled.
+*/
 
 var calc = function (expression) {
-
-//simple calculator. string input not checked, may contain ()+-*/    
 
     function calculate(m, a, b) {
     switch (m) {                        
@@ -19,7 +63,7 @@ var calc = function (expression) {
   
     while (/\(/.test(exp)) {
       exp = exp.replace(regPar, (match,p1)=>calc(p1));  //resolve (nested) parentheses
-      exp = exp.replace(/([-+*/])\s*\-{2}/g, '$1');     //remove '--'
+      exp = exp.replace(/([-+*/]|^)\s*\-{2}/g, '$1');     //remove '--'
      }
   
     while (/[*/]/.test(exp)) {              //calc * /
